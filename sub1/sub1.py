78352256944ee9055f79ffe5088cd1cadd47a9eb
MASHOD0 <mashhood2002@gmail.com>
2022-02-18 11:08:40 +0530

    final submission

--- sub1/sub1.py
+++ sub1/sub1.py
@@ -1,23 +1,30 @@
 def main():
-    i = int(input().split()[0])
-    likes = []
-    dislikes = []
-    for a in range(i):
+    try:
         
-        line = input().split()
-        for b in range(int(line[0])):
-            if line[b+1] not in likes:
-                likes.append(line[b+1])
-        line = input().split()
-        for c in range(int(line[0])):
-            if line[c+1] not in dislikes:
-                dislikes.append(line[c+1])
-    
-    toppings = gettoppings(likes, dislikes)
-    print(len(toppings), end=' ')
-    for i in range(len(toppings)):
-        print(toppings[i], end=' ')
+        f = open("a_an_example.in.txt", 'r')
+        i = int(f.readline().split()[0])
+        likes = []
+        dislikes = []
+        for a in range(i):
+            
+            line = f.readline().split()
+            for b in range(int(line[0])):
+                if line[b+1] not in likes:
+                    likes.append(line[b+1])
+            line = f.readline().split()
+            for c in range(int(line[0])):
+                if line[c+1] not in dislikes:
+                    dislikes.append(line[c+1])
+        toppings = gettoppings(likes, dislikes)
 
+        with open("a_an_example.out.txt",'w',encoding = 'utf-8') as w:
+            w.write(f"{len(toppings)} ")
+            for i in range(len(toppings)):
+                w.write(f"{toppings[i]} ")
+        
+        
+    finally:
+        f.close()
 def gettoppings(likes, dislikes):
     toppings = []
     for i in range(len(likes)):
